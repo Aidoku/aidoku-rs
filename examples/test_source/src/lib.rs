1591b4180fb3ba99212125658a1f95d70bd8e24f
Magnetar <contact@magnetar.dev>
2022-04-02 00:28:11 -0400

    feat(std): finish std impl, remove non v0.2 features

--- examples/test_source/src/lib.rs
+++ examples/test_source/src/lib.rs
@@ -6,3 +6,8 @@
 pub unsafe fn panic_handle(_info: &core::panic::PanicInfo) -> ! {
     core::intrinsics::abort()
 }
+
+extern crate wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
