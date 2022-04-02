da937a914583a36e5d562210ad4f0135d87a5bad
Magnetar <contact@magnetar.dev>
2022-04-02 00:56:50 -0400

    fix(json): cleanup import name

--- crates/aidoku_imports/src/json.rs
+++ crates/aidoku_imports/src/json.rs
@@ -3,10 +3,10 @@ use super::std::{Rid, ValueRef};
 #[link(wasm_import_module = "json")]
 extern "C" {
     #[link_name = "parse"]
-    fn __wasm_parse(bytes: *const u8, size: usize) -> Rid;
+    fn json_parse(bytes: *const u8, size: usize) -> Rid;
 }
 
 pub fn parse(buf: &[u8]) -> ValueRef {
-    let rid = unsafe { __wasm_parse(buf.as_ptr(), buf.len()) };
+    let rid = unsafe { json_parse(buf.as_ptr(), buf.len()) };
     ValueRef::new(rid)
 }
