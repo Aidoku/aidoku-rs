e68dd02e25e853b64983e1c76dcb3d16fd34f096
Magnetar <contact@magnetar.dev>
2022-04-02 00:56:08 -0400

    fix(net): clean up method names

--- crates/aidoku_imports/src/net.rs
+++ crates/aidoku_imports/src/net.rs
@@ -18,30 +18,15 @@ pub enum HttpMethod {
 
 #[link(wasm_import_module = "net")]
 extern "C" {
-    #[link_name = "request_init"]
-    fn __wasm_request_init(method: HttpMethod) -> Rid;
-    #[link_name = "request_set_url"]
-    fn __wasm_request_set_url(rd: Rid, value: *const u8, len: usize);
-    #[link_name = "request_set_header"]
-    fn __wasm_request_set_header(
-        rd: Rid,
-        key: *const u8,
-        key_len: usize,
-        val: *const u8,
-        val_len: usize,
-    );
-    #[link_name = "request_set_body"]
-    fn __wasm_request_set_body(rd: Rid, value: *const u8, len: usize);
-    #[link_name = "request_send"]
-    fn __wasm_request_send(rd: Rid);
-    #[link_name = "request_get_data"]
-    fn __wasm_request_get_data(rd: Rid, buffer: *mut u8, size: usize);
-    #[link_name = "request_get_data_size"]
-    fn __wasm_request_get_data_size(rd: Rid) -> usize;
-    #[link_name = "request_close"]
-    fn __wasm_request_close(rd: Rid);
-    #[link_name = "request_json"]
-    fn __wasm_request_json(rd: Rid) -> ValueRid;
+    fn request_init(method: HttpMethod) -> Rid;
+    fn request_set_url(rd: Rid, value: *const u8, len: usize);
+    fn request_set_header(rd: Rid, key: *const u8, key_len: usize, val: *const u8, val_len: usize);
+    fn request_set_body(rd: Rid, value: *const u8, len: usize);
+    fn request_send(rd: Rid);
+    fn request_get_data(rd: Rid, buffer: *mut u8, size: usize);
+    fn request_get_data_size(rd: Rid) -> usize;
+    fn request_close(rd: Rid);
+    fn request_json(rd: Rid) -> ValueRid;
 }
 
 /// A type that makes a HTTP request.
@@ -55,8 +40,8 @@ impl Request {
     /// ```
     pub fn new(url: &str, method: HttpMethod) -> Self {
         unsafe {
-            let rd = __wasm_request_init(method);
-            __wasm_request_set_url(rd, url.as_ptr(), url.len());
+            let rd = request_init(method);
+            request_set_url(rd, url.as_ptr(), url.len());
             Self(rd)
         }
     }
@@ -64,29 +49,29 @@ impl Request {
     /// Set a header
     pub fn header(self, key: &str, val: &str) -> Self {
         unsafe {
-            __wasm_request_set_header(self.0, key.as_ptr(), key.len(), val.as_ptr(), val.len());
+            request_set_header(self.0, key.as_ptr(), key.len(), val.as_ptr(), val.len());
         };
         self
     }
 
     /// Set the body's data
     pub fn body(self, data: &[u8]) -> Self {
-        unsafe { __wasm_request_set_body(self.0, data.as_ptr(), data.len()) };
+        unsafe { request_set_body(self.0, data.as_ptr(), data.len()) };
         self
     }
 
     fn send(&self) {
-        unsafe { __wasm_request_send(self.0) }
+        unsafe { request_send(self.0) }
     }
 
     fn close(&self) {
-        unsafe { __wasm_request_close(self.0) }
+        unsafe { request_close(self.0) }
     }
 
     /// Get the raw data from the response
     pub fn data<'a>(self) -> Vec<u8> {
         self.send();
-        let size = unsafe { __wasm_request_get_data_size(self.0) };
+        let size = unsafe { request_get_data_size(self.0) };
         let mut buffer: Vec<u8> = Vec::with_capacity(size);
         let mut offset: usize = 0;
         while offset < size {
@@ -96,7 +81,7 @@ impl Request {
             } else {
                 &mut buffer[offset..]
             };
-            unsafe { __wasm_request_get_data(self.0, chunk.as_mut_ptr(), chunk.len()) }
+            unsafe { request_get_data(self.0, chunk.as_mut_ptr(), chunk.len()) }
             offset = ending_offset;
         }
         self.close();
@@ -106,7 +91,7 @@ impl Request {
     /// Get the data as JSON
     pub fn json(self) -> ValueRef {
         self.send();
-        let rid = unsafe { __wasm_request_json(self.0) };
+        let rid = unsafe { request_json(self.0) };
         self.close();
         ValueRef::new(rid)
     }
