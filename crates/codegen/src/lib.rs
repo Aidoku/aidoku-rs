eaba0ef4bb4d304c9a62df9662665114faa38de7
Magnetar <contact@magnetar.dev>
2022-04-10 19:26:41 -0400

    feat(codegen): usable macros

--- crates/codegen/src/lib.rs
+++ crates/codegen/src/lib.rs
@@ -3,19 +3,64 @@ use quote::quote;
 use syn::{parse, ItemFn};
 
 #[proc_macro_attribute]
-pub fn manga_list_request(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get_manga_list(_: TokenStream, input: TokenStream) -> TokenStream {
     let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
     let func_name = &func.sig.ident;
     quote! {
         #func
-        #[no_mangle]
-        pub unsafe extern "C" fn manga_list_request(filters_rid: i32, page: i32) -> i32 {
-            use aidoku::std::Vec;
-            let mut filters: Vec<> = Vec::new();
 
-            let resp: Result<aidoku::MangaPageResult> = #func_name(filters, page);
+        #[no_mangle]
+        #[export_name = "get_manga_list"]
+        pub unsafe extern "C" fn __wasm_get_manga_list(filters_rid: i32, page: i32) -> i32 {
+            let mut filters: Vec<Filter> = Vec::new();
+            if filters_rid > -1 {
+                let filters_ref = aidoku::std::ValueRef::new(filters_rid);
+                if let Ok(arr) = filters_ref.as_array() {
+                    for item in arr {
+                        let filter_ref = match item.as_object() {
+                            Ok(filter_ref) => filter_ref,
+                            Err(_) => continue,
+                        };
+                        let name = match filter_ref.get("name").as_string() {
+                            Ok(name) => name,
+                            Err(_) => continue,
+                        };
+                        let filter = Filter {
+                            kind: aidoku::FilterType::from_i64(filter_ref.get("type").as_int().unwrap_or(0)),
+                            name: name.read(),
+                            value: filter_ref.get("value")
+                        };
+                        filters.push(filter);
+                    }
+                }
+            }
+            let resp: aidoku::error::Result<aidoku::MangaPageResult> = #func_name(filters, page);
             match resp {
-                Ok(resp) => resp,
+                Ok(resp) => resp.create(),
+                Err(_) => -1,
+            }
+        }
+    }.into()
+}
+
+#[proc_macro_attribute]
+pub fn get_manga_listing(_: TokenStream, input: TokenStream) -> TokenStream {
+    let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
+    let func_name = &func.sig.ident;
+    quote! {
+        #func
+
+        #[no_mangle]
+        #[export_name = "get_manga_listing"]
+        pub unsafe extern "C" fn __wasm_get_manga_listing(listing_rid: i32, page: i32) -> i32 {
+            let name = match aidoku::std::ObjectRef(listing_rid).get("name").as_string() {
+                Ok(name) => name,
+                Err(_) => return -1,
+            };
+            let listing = Listing { name: name.read() };
+            let resp: Result<MangaPageResult> = #func_name(listing, page);
+            match resp {
+                Ok(resp) => resp.create(),
                 Err(_) => -1,
             }
         }
@@ -24,16 +69,22 @@ pub fn manga_list_request(_: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 #[proc_macro_attribute]
-pub fn manga_listing_request(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get_manga_details(_: TokenStream, input: TokenStream) -> TokenStream {
     let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
     let func_name = &func.sig.ident;
     quote! {
         #func
+
         #[no_mangle]
-        pub unsafe extern "C" fn manga_listing_request(filters: *const i32, page: i32) -> i32 {
-            let resp = #func_name();
+        #[export_name = "get_manga_details"]
+        pub unsafe extern "C" fn __wasm_get_manga_details(manga_rid: i32) -> i32 {
+            let id = match aidoku::std::ObjectRef(manga_rid).get("id").as_string() {
+                Ok(id) => id,
+                Err(_) => return -1,
+            };
+            let resp: Result<Manga> = #func_name(id.read());
             match resp {
-                Ok(resp) => resp,
+                Ok(resp) => resp.create(),
                 Err(_) => -1,
             }
         }
@@ -42,16 +93,29 @@ pub fn manga_listing_request(_: TokenStream, input: TokenStream) -> TokenStream 
 }
 
 #[proc_macro_attribute]
-pub fn manga_details_request(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get_chapter_list(_: TokenStream, input: TokenStream) -> TokenStream {
     let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
     let func_name = &func.sig.ident;
     quote! {
         #func
+
         #[no_mangle]
-        pub unsafe extern "C" fn manga_details_request(filters: *const i32, page: i32) -> i32 {
-            let resp = #func_name();
+        #[export_name = "get_chapter_list"]
+        pub unsafe extern "C" fn __wasm_get_chapter_list(manga_rid: i32) -> i32 {
+            let id = match aidoku::std::ObjectRef(manga_rid).get("id").as_string() {
+                Ok(id) => id,
+                Err(_) => return -1,
+            };
+            let resp: Result<Vec<Chapter>> = #func_name(id.read());
             match resp {
-                Ok(resp) => resp,
+                Ok(resp) => {
+                    let mut arr = aidoku::std::ArrayRef::new();
+                    for item in resp {
+                        let rid = item.create();
+                        arr.insert(aidoku::std::ValueRef::new(rid));
+                    }
+                    arr.0
+                }
                 Err(_) => -1,
             }
         }
@@ -60,34 +124,29 @@ pub fn manga_details_request(_: TokenStream, input: TokenStream) -> TokenStream 
 }
 
 #[proc_macro_attribute]
-pub fn chapter_list_request(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get_page_list(_: TokenStream, input: TokenStream) -> TokenStream {
     let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
     let func_name = &func.sig.ident;
     quote! {
         #func
-        #[no_mangle]
-        pub unsafe extern "C" fn chapter_list_request(filters: *const i32, page: i32) -> i32 {
-            let resp = #func_name();
-            match resp {
-                Ok(resp) => resp,
-                Err(_) => -1,
-            }
-        }
-    }
-    .into()
-}
 
-#[proc_macro_attribute]
-pub fn page_list_request(_: TokenStream, input: TokenStream) -> TokenStream {
-    let func: ItemFn = parse(input).expect("expected the attribute to be used on a function");
-    let func_name = &func.sig.ident;
-    quote! {
-        #func
         #[no_mangle]
-        pub unsafe extern "C" fn page_list_request(chapter: *const i32) -> i32 {
-            let resp = #func_name();
+        #[export_name = "get_page_list"]
+        pub unsafe extern "C" fn __wasm_get_page_list(chapter_rid: i32) -> i32 {
+            let id = match aidoku::std::ObjectRef(chapter_rid).get("id").as_string() {
+                Ok(id) => id,
+                Err(_) => return -1,
+            };
+            let resp: Result<Vec<Page>> = #func_name(id.read());
             match resp {
-                Ok(resp) => resp,
+                Ok(resp) => {
+                    let mut arr = aidoku::std::ArrayRef::new();
+                    for item in resp {
+                        let rid = item.create();
+                        arr.insert(aidoku::std::ValueRef::new(rid));
+                    }
+                    arr.0
+                }
                 Err(_) => -1,
             }
         }
--- examples/test_source/src/lib.rs
+++ examples/test_source/src/lib.rs
@@ -1,123 +1,10 @@
 #![no_std]
 use aidoku::{
-    error::Result,
-    std::String,
-    // prelude::*,
-    std::Vec,
-    Filter,
-    Listing,
-    Manga,
-    MangaPageResult,
-    Chapter,
-    Page,
+    error::Result, prelude::*, std::String, std::Vec, Chapter, Filter, Listing, Manga,
+    MangaPageResult, Page,
 };
 
-#[no_mangle]
-#[export_name = "get_manga_list"]
-pub unsafe extern "C" fn __wasm_get_manga_list(filters_rid: i32, page: i32) -> i32 {
-    let mut filters: Vec<Filter> = Vec::new();
-    if filters_rid > -1 {
-        let filters_ref = aidoku::std::ValueRef::new(filters_rid);
-        if let Ok(arr) = filters_ref.as_array() {
-            for item in arr {
-                let filter_ref = match item.as_object() {
-                    Ok(filter_ref) => filter_ref,
-                    Err(_) => continue,
-                };
-                let name = match filter_ref.get("name").as_string() {
-                    Ok(name) => name,
-                    Err(_) => continue,
-                };
-                let filter = Filter {
-                    kind: aidoku::FilterType::from_i64(filter_ref.get("type").as_int().unwrap_or(0)),
-                    name: name.read(),
-                    value: filter_ref.get("value")
-                };
-                filters.push(filter);
-            }
-        }
-    }
-    let resp: aidoku::error::Result<aidoku::MangaPageResult> = get_manga_list(filters, page);
-    match resp {
-        Ok(resp) => resp.create(),
-        Err(_) => -1,
-    }
-}
-
-#[no_mangle]
-#[export_name = "get_manga_listing"]
-pub unsafe extern "C" fn __wasm_get_manga_listing(listing_rid: i32, page: i32) -> i32 {
-    let name = match aidoku::std::ObjectRef(listing_rid).get("name").as_string() {
-        Ok(name) => name,
-        Err(_) => return -1,
-    };
-    let listing = Listing {
-        name: name.read()
-    };
-    let resp: Result<MangaPageResult> = get_manga_listing(listing, page);
-    match resp {
-        Ok(resp) => resp.create(),
-        Err(_) => -1,
-    }
-}
-
-#[no_mangle]
-#[export_name = "get_manga_details"]
-pub unsafe extern "C" fn __wasm_get_manga_details(manga_rid: i32) -> i32 {
-    let id = match aidoku::std::ObjectRef(manga_rid).get("id").as_string() {
-        Ok(id) => id,
-        Err(_) => return -1,
-    };
-    let resp: Result<Manga> = get_manga_details(id.read());
-    match resp {
-        Ok(resp) => resp.create(),
-        Err(_) => -1,
-    }
-}
-
-#[no_mangle]
-#[export_name = "get_chapter_list"]
-pub unsafe extern "C" fn __wasm_get_chapter_list(manga_rid: i32) -> i32 {
-    let id = match aidoku::std::ObjectRef(manga_rid).get("id").as_string() {
-        Ok(id) => id,
-        Err(_) => return -1,
-    };
-    let resp: Result<Vec<Chapter>> = get_chapter_list(id.read());
-    match resp {
-        Ok(resp) => {
-            let mut arr = aidoku::std::ArrayRef::new();
-            for item in resp {
-                let rid = item.create();
-                arr.insert(aidoku::std::ValueRef::new(rid));
-            }
-            arr.0
-        },
-        Err(_) => -1,
-    }
-}
-
-#[no_mangle]
-#[export_name = "get_page_list"]
-pub unsafe extern "C" fn __wasm_get_page_list(chapter_rid: i32) -> i32 {
-    let id = match aidoku::std::ObjectRef(chapter_rid).get("id").as_string() {
-        Ok(id) => id,
-        Err(_) => return -1,
-    };
-    let resp: Result<Vec<Page>> = get_page_list(id.read());
-    match resp {
-        Ok(resp) => {
-            let mut arr = aidoku::std::ArrayRef::new();
-            for item in resp {
-                let rid = item.create();
-                arr.insert(aidoku::std::ValueRef::new(rid));
-            }
-            arr.0
-        },
-        Err(_) => -1,
-    }
-}
-
-// #[manga_list_request]
+#[get_manga_list]
 fn get_manga_list<'a>(_: Vec<Filter>, _: i32) -> Result<MangaPageResult<'a>> {
     let mut manga: Vec<Manga> = Vec::new();
     manga.push(Manga {
@@ -134,11 +21,12 @@ fn get_manga_list<'a>(_: Vec<Filter>, _: i32) -> Result<MangaPageResult<'a>> {
         viewer: aidoku::MangaViewer::Default,
     });
     Ok(MangaPageResult {
-        manga: manga,
+        manga,
         has_more: false,
     })
 }
 
+#[get_manga_listing]
 fn get_manga_listing<'a>(_: Listing, _: i32) -> Result<MangaPageResult<'a>> {
     Ok(MangaPageResult {
         manga: Vec::new(),
@@ -146,14 +34,23 @@ fn get_manga_listing<'a>(_: Listing, _: i32) -> Result<MangaPageResult<'a>> {
     })
 }
 
+#[get_manga_details]
 fn get_manga_details<'a>(_: String) -> Result<Manga<'a>> {
-    Err(aidoku::error::AidokuError { reason: aidoku::error::AidokuErrorKind::Unimplemented })
+    Err(aidoku::error::AidokuError {
+        reason: aidoku::error::AidokuErrorKind::Unimplemented,
+    })
 }
 
+#[get_chapter_list]
 fn get_chapter_list<'a>(_: String) -> Result<Vec<Chapter<'a>>> {
-    Err(aidoku::error::AidokuError { reason: aidoku::error::AidokuErrorKind::Unimplemented })
+    Err(aidoku::error::AidokuError {
+        reason: aidoku::error::AidokuErrorKind::Unimplemented,
+    })
 }
 
+#[get_page_list]
 fn get_page_list<'a>(_: String) -> Result<Vec<Page<'a>>> {
-    Err(aidoku::error::AidokuError { reason: aidoku::error::AidokuErrorKind::Unimplemented })
+    Err(aidoku::error::AidokuError {
+        reason: aidoku::error::AidokuErrorKind::Unimplemented,
+    })
 }
