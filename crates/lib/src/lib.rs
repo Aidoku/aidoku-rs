09ce18a1e67a2c827a0ad3d1600bafb422530e74
beerpiss <lacvtg.a1.2023@gmail.com>
2022-06-14 15:38:58 +0700

    reintroduce abort

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![feature(core_intrinsics, alloc_error_handler, fmt_internals)]
+#![feature(core_intrinsics, alloc_error_handler, fmt_internals, panic_info_message)]
 
 // Setup allocator
 
@@ -11,9 +11,66 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // Set panic handlers
 
-#[cfg_attr(not(test), panic_handler)]
-pub fn panic_handle(_info: &core::panic::PanicInfo) -> ! {
+pub fn abort<T: AsRef<str>>(message: T, file: T, line: u32, column: u32) -> ! {
+    extern "C" { 
+        #[link_name = "abort"]
+        fn _abort(message: *const u8, file: *const u8, line: i32, column: i32); 
+    }
+    extern crate alloc;
+    use core::{alloc::Layout, ptr::copy};
+    use alloc::alloc::alloc_zeroed;
+
+    let message = message.as_ref();
+    let file = file.as_ref();
+
+    // Basically, AssemblyScript places 4 bytes before the string slice to denote
+    // its length. This is why we need the extra 8 bytes.
+    let layout = Layout::from_size_align(
+        8 + message.len() + file.len(), 
+        core::mem::align_of::<u8>()
+    ).unwrap();
+
+    unsafe {
+        let message_len_ptr = alloc_zeroed(layout) as *mut i32;
+        *message_len_ptr = i32::try_from(message.len()).unwrap_or(-1);
+
+        let message_ptr = message_len_ptr.add(1) as *mut u8;
+        copy::<u8>(message.as_ptr(), message_ptr, message.len());
+
+        let file_len_ptr = message_len_ptr.add(message.len()) as *mut i32;
+        *file_len_ptr = i32::try_from(file.len()).unwrap_or(-1);
+
+        let file_ptr = file_len_ptr.add(1) as *mut u8;
+        copy::<u8>(file.as_ptr(), file_ptr, file.len());
+        
+        _abort(
+            message_ptr, 
+            file_ptr, 
+            i32::try_from(line).unwrap_or(-1), 
+            i32::try_from(column).unwrap_or(-1)
+        )
+    }
     core::intrinsics::abort()
+}
+
+#[cfg_attr(not(test), panic_handler)]
+pub fn panic_handle(info: &core::panic::PanicInfo) -> ! {
+    use aidoku_imports::Write;
+    let (file, line, col) = if let Some(location) = info.location() {
+        (location.file(), location.line(), location.column())
+    } else {
+        ("", 0, 0)
+    };
+
+    let message = if let Some(args) = info.message() {
+        let mut string = crate::std::String::with_capacity(args.estimated_capacity());
+        string.write_fmt(*args).unwrap_or_default();
+        string
+    } else {
+        crate::std::String::new()
+    };
+
+    abort(message, crate::std::String::from(file), line, col)
 }
 
 #[cfg_attr(not(test), alloc_error_handler)]
