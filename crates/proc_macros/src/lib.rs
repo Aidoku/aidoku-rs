cc4d6a8dd3b44e266d2e6acbd7f199bc2eff24de
beerpiss <lacvtg.a1.2023@gmail.com>
2022-08-01 22:10:45 +0700

    fix imports

--- crates/proc_macros/src/lib.rs
+++ crates/proc_macros/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse, Data, DeriveInput, Ident, ItemFn, Type};
+use syn::{parse, ItemFn};
 
 #[proc_macro_attribute]
 pub fn initialize(_: TokenStream, input: TokenStream) -> TokenStream {
