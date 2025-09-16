55cfa0bd2418ee1c12a175f352867882614d232e
Aphronyx <64679454+aphronyx@users.noreply.github.com>
2025-09-16 23:34:17 +0800

    feat(lib): add markdown escaping helper

--- crates/lib/src/helpers/string.rs
+++ crates/lib/src/helpers/string.rs
@@ -1,3 +1,7 @@
+extern crate alloc;
+
+use alloc::string::String;
+
 pub trait StripPrefixOrSelf {
 	fn strip_prefix_or_self<P: AsRef<str>>(&self, prefix: P) -> &str;
 }
@@ -5,5 +9,22 @@ pub trait StripPrefixOrSelf {
 impl StripPrefixOrSelf for str {
 	fn strip_prefix_or_self<P: AsRef<str>>(&self, prefix: P) -> &str {
 		self.strip_prefix(prefix.as_ref()).unwrap_or(self)
+	}
+}
+
+pub trait PlaintText {
+	fn escape_markdown(&self) -> String;
+}
+
+impl<S: AsRef<str>> PlaintText for S {
+	fn escape_markdown(&self) -> String {
+		let mut markdown = String::new();
+		for char in self.as_ref().chars() {
+			if char.is_ascii_punctuation() {
+				markdown.push('\\');
+			}
+			markdown.push(char);
+		}
+		markdown.replace("\r\n", "\n").replace('\n', "  \n")
 	}
 }
