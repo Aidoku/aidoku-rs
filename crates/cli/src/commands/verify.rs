339b240f49d7d07e6fb77d722b53ab6bfce3961c
Skitty <mrskittyblock@gmail.com>
2026-05-04 10:49:25 +0900

    feat(cli): basic wasm binary verification
    checks for minimum required exported function names and ensures minimum app version is set when using imported api functions that are version gated
    
    also updates dependencies

--- crates/cli/src/commands/verify.rs
+++ crates/cli/src/commands/verify.rs
@@ -1,9 +1,12 @@
 //! Verify an Aidoku source to ensure it's ready to be published.
+use crate::models::SourceJson;
 use anyhow::anyhow;
 use colored::Colorize;
 use image::GenericImageView;
+use semver::Version;
 use serde_json::Value;
 use std::io::prelude::*;
+use wasmparser::{Parser, Payload};
 
 // include json schemas as static strings
 const SOURCE_JSON_SCHEMA: &str = include_str!("../supporting/schema/source.schema.json");
@@ -42,12 +45,23 @@ pub fn run(files: Vec<std::path::PathBuf>) -> anyhow::Result<()> {
 			continue;
 		};
 
+		let source_json: Option<SourceJson> = {
+			let mut contents = String::new();
+			if archive
+				.by_name("Payload/source.json")
+				.map(|mut file| file.read_to_string(&mut contents).is_ok())
+				.unwrap_or(false)
+			{
+				serde_json::from_str(&contents).ok()
+			} else {
+				None
+			}
+		};
+
 		// main.wasm
-		let has_main_wasm = if archive.by_name("Payload/main.wasm").is_ok() {
+		let has_main_wasm = if let Ok(file) = archive.by_name("Payload/main.wasm") {
 			println!("  * main.wasm");
-			// todo: do some verification here
-			println!("    * note: the executable itself is not verified");
-			true
+			validate_wasm(file, source_json)
 		} else {
 			println!("  {}", "* missing main.wasm".red());
 			false
@@ -186,4 +200,139 @@ fn validate_json(
 	} else {
 		true
 	}
+}
+
+// the mimimum exports required for a source to work
+#[derive(Default)]
+struct RequiredExports {
+	start: bool,
+	free_result: bool,
+	get_search_manga_list: bool,
+	get_manga_update: bool,
+	get_page_list: bool,
+}
+
+impl RequiredExports {
+	fn new() -> Self {
+		Self::default()
+	}
+
+	fn all_satisfied(&self) -> bool {
+		self.start
+			&& self.free_result
+			&& self.get_search_manga_list
+			&& self.get_manga_update
+			&& self.get_page_list
+	}
+
+	fn mark(&mut self, name: &str) {
+		match name {
+			"start" => self.start = true,
+			"free_result" => self.free_result = true,
+			"get_search_manga_list" => self.get_search_manga_list = true,
+			"get_manga_update" => self.get_manga_update = true,
+			"get_page_list" => self.get_page_list = true,
+			_ => {}
+		}
+	}
+}
+
+fn validate_wasm(
+	mut wasm_file: zip::read::ZipFile<'_, std::io::BufReader<std::fs::File>>,
+	source_json: Option<SourceJson>,
+) -> bool {
+	let mut wasm_bytes = Vec::new();
+	if !wasm_file.read_to_end(&mut wasm_bytes).is_ok() {
+		return false;
+	}
+
+	let mut exports = RequiredExports::new();
+	let mut api_min_version = Version::new(0, 7, 0);
+
+	let mut exports_checked = false;
+	let mut imports_checked = false;
+
+	for payload in Parser::new(0).parse_all(&wasm_bytes) {
+		let Ok(payload) = payload else {
+			continue;
+		};
+		match payload {
+			Payload::ImportSection(s) => {
+				for import in s.into_imports() {
+					let Ok(import) = import else {
+						continue;
+					};
+					use wasmparser::Import;
+					let v = match import {
+						Import {
+							module: "html",
+							name: "kind" | "child_nodes",
+							..
+						} => Version::new(0, 8, 3),
+						Import {
+							module: "net",
+							name: "get_url" | "set_timeout",
+							..
+						} => Version::new(0, 8, 3),
+						Import {
+							module: "html",
+							name:
+								"remove" | "add_class" | "remove_class" | "set_attr" | "remove_attr",
+							..
+						} => Version::new(0, 8, 0),
+						Import {
+							module: "std",
+							name: "parse_date",
+							..
+						} => Version::new(0, 7, 1),
+						_ => continue,
+					};
+					if v > api_min_version {
+						api_min_version = v;
+					}
+				}
+				imports_checked = true;
+			}
+			Payload::ExportSection(s) => {
+				for export in s {
+					let Ok(export) = export else {
+						continue;
+					};
+					exports.mark(export.name);
+				}
+				exports_checked = true;
+			}
+			_ => continue,
+		}
+		if exports_checked && imports_checked {
+			break;
+		}
+	}
+
+	let defined_min_version = source_json
+		.and_then(|json| json.info.min_app_version)
+		.and_then(|v| Version::parse(&v).ok())
+		.unwrap_or(Version::new(0, 7, 0));
+
+	let api_valid = api_min_version <= defined_min_version;
+	println!(
+		"    * defined minimum version accepts api version... {}",
+		if api_valid {
+			"yes".green()
+		} else {
+			format!("no (api version: {api_min_version})").red()
+		}
+	);
+
+	let exports_valid = exports.all_satisfied();
+	println!(
+		"    * minimum functions exported... {}",
+		if exports_valid {
+			"yes".green()
+		} else {
+			"no".red()
+		}
+	);
+
+	api_valid && exports_valid
 }
